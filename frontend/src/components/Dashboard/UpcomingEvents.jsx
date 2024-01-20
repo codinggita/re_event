a4ffbb156db4ef60b675610217c18ae9c5e22090
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-01-20 15:54:18 +0530

    added eventcard and event list page

--- frontend/src/components/Dashboard/UpcomingEvents.jsx
+++ frontend/src/components/Dashboard/UpcomingEvents.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import EventCard from './EventCard';
 
 const UpcomingEvents = () => {
   return (
     <>
-      <div className="w-full flex p-10 text-white">
-        <h2 className="text-2xl font-bold">Upcoming Events</h2>
+      <div className="w-full flex p-10 items-center justify-center text-white">
+        <div className="w-2/3 flex flex-col">
+          <h1 className='text-lg text-start'>Upcoming Events</h1>
+          <div className="flex py-8 px-4">
+            <div className="w-1/4">
+              Date
+            </div>
+            <div className="w-3/4 flex flex-col gap-4">
+              <EventCard eventname="How to roast people (dark edition) 💗" location="Bora Bora Islands" time="10.30 AM" organiser="Bals Shiva"/>
+              <EventCard eventname="How to fuck people (cute edition) 💗" location="Republican of Congo" time="10.30 AM" organiser="Bals Shiva"/>
+              {/* <EventCard /> */}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
--- frontend/src/components/Login/LogSign.jsx
+++ frontend/src/components/Login/LogSign.jsx
@@ -1,32 +1,38 @@
-import React, { useState, useEffect, useRef } from "react";
-import { FaGoogle } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import { FaGoogle, FaChevronLeft } from "react-icons/fa";
 import OtpInput from "react-otp-input";
-import { FaChevronLeft } from "react-icons/fa";
+import { toast } from "sonner";
 
 const LogSign = () => {
   const [oncontinue, setOncontinue] = useState(false);
   const [otp, setOtp] = useState("");
 
+  useEffect(() => {
+    if (otp.length === 5) {
+      handleSubmit();
+    }
+  }, [otp]);
+
+  const handleSubmit = () => {
+    console.log(otp);
+    toast.success("Osama bin laden");
+  };
+
   return (
     <>
-      <div className=" absolute  backdrop-blur-lg  shadow-xl border-white/70  border  px-8 py-12  rounded-3xl  bg-[#212325]/80 text-white  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute backdrop-blur-lg shadow-xl border-white/70 border px-8 py-12 rounded-3xl bg-[#212325]/80 text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         {!oncontinue ? (
-          <div className="flex flex-col  items-center justify-between  gap-10 ">
-            {/* <img
-              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-              alt="logo"
-              className="w-1/2 invert  mx-auto"
-            /> */}
-            <h1 className=" text-5xl">re:Event</h1>
-            <div className=" flex flex-col  items-center gap-2">
-              <h1 className="  text-3xl">Welcome to re:Event</h1>
-              <h2 className=" text-md">Please use your email below</h2>
+          <div className="flex flex-col items-center justify-between gap-10">
+            <h1 className="text-5xl">re:Event</h1>
+            <div className="flex flex-col items-center gap-2">
+              <h1 className="text-3xl">Welcome to re:Event</h1>
+              <h2 className="text-md">Please use your email below</h2>
             </div>
             <div className="flex flex-col">
-              <h1 className=" mb-2  tracking-wider text-sm font-bold">Email</h1>
-              <form className=" flex flex-col gap-5">
+              <h1 className="mb-2 tracking-wider text-sm font-bold">Email</h1>
+              <form className="flex flex-col gap-5">
                 <input
-                  className=" focus:border-1 outline-none bg-transparent border-gray-500 rounded-md pl-4 w-[300px] py-2 border-[1px] "
+                  className="focus:border-1 outline-none bg-transparent border-gray-500 rounded-md pl-4 w-[300px] py-2 border-[1px]"
                   type="text"
                   placeholder="email"
                 />
@@ -36,36 +42,33 @@ const LogSign = () => {
                 >
                   Continue with Email
                 </button>
-                <hr className="    border-gray-400  " />
-                <div className=" rounded-md flex bg-gray-700 px-8 py-2 items-center justify-center gap-2">
+                <hr className="border-gray-400" />
+                <div className="rounded-md flex bg-gray-700 px-8 py-2 items-center justify-center gap-2">
                   <FaGoogle className="text-gray-200" />
-                  <button className="    text-gray-200 ">
-                    Continue with Google
-                  </button>
+                  <button className="text-gray-200 ">Continue with Google</button>
                 </div>
               </form>
             </div>
           </div>
         ) : (
           <>
-            <div className=" flex flex-col gap-5  ">
+            <div className="flex flex-col gap-5 ">
               <div
-                className="rounded-xl  flex items-center cursor-pointer justify-center -mt-6 border  border-gray-500 w-8 h-8"
+                className="rounded-xl flex items-center cursor-pointer justify-center -mt-6 border border-gray-500 w-8 h-8"
                 onClick={() => setOncontinue(false)}
               >
-                <FaChevronLeft className="  text-lg  text-white/60 " />
+                <FaChevronLeft className="text-lg text-white/60 " />
               </div>
-
-              <h1 className="text-2xl  text-white">Enter Code </h1>
+              <h1 className="text-2xl text-white">Enter Code </h1>
               <h1>{otp}</h1>
-              <div className=" flex flex-col gap-1 mb-5">
-                <h2 className=" text-sm text-white/80">
-                  please enter the 5 digit code sent to{" "}
+              <div className="flex flex-col gap-1 mb-5">
+                <h2 className="text-sm text-white/80">
+                  Please enter the 5 digit code sent to{" "}
                 </h2>
                 <h3 className="text-sm">shivatadigadapa@gmail.com</h3>
               </div>
             </div>
-            <div className="flex flex-col    items-center justify-center gap-10">
+            <div className="flex flex-col items-center justify-center gap-10">
               <form className="w-full flex items-center justify-center flex-col">
                 <OtpInput
                   value={otp}
@@ -76,7 +79,6 @@ const LogSign = () => {
                   inputStyle={{
                     width: "40px",
                     height: "40px",
-                    // outline:"none",
                     border: "1px",
                     borderRadius: "5px",
                     backgroundColor: "black",
@@ -85,12 +87,15 @@ const LogSign = () => {
                     fontWeight: "bold",
                   }}
                 />
-                <button
-                  type="submit"
-                  className="mt-4 border bg-gray-600  border-gray-500 text-white py-2 px-4 rounded-md shadow-md font-semibold tracking-wider"
-                >
-                  ReSend code
-                </button>
+                <div className="flex gap-2 mt-4">
+                  <button
+                    type="button"
+                    onClick={handleSubmit}
+                    className="mt-4 border bg-gray-600 border-gray-500 text-white py-2 px-4 rounded-md shadow-md font-semibold tracking-wider"
+                  >
+                    ReSend code
+                  </button>
+                </div>
               </form>
             </div>
           </>
